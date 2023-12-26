b08db71d55e7e9657f4a0d026d0e40291fb652cb
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-12-26 21:18:29 +0300

    2 task

---
+++ src/ArraySchema.js
@@ -0,0 +1,17 @@
+export default class ArraySchema {
+    validators = [(val) => Array.isArray(val)];
+  
+    constructor (newValidators){
+      if(newValidators) this.validators = newValidators;
+    }
+  
+    isValid(val) {
+      return this.validators.every( validator => validator(val));
+    }
+  
+    allIntegers(){
+      const validator = (array) => array.every( element => Number.isInteger(element));
+      return new ArraySchema([...this.validators, validator]); 
+    }
+    
+  }
--- src/Validator.js
+++ src/Validator.js
@@ -1,8 +1,11 @@
 import NumberSchema from './NumberSchema.js'
+import ArraySchema from './ArraySchema.js'
 
 export default class Validator {
   number() {
     return new NumberSchema();
   }
-  
+  array(){
+    return new ArraySchema();
+  }
 } 
